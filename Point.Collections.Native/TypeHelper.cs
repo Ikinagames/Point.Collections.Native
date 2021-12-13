ea3929f739a62891cfe4cdf762065c6b092f853b
Syadeu (Syadeu Home) <syadeu@gmail.com>
2021-12-14 03:39:44 +0900

    update license

--- Point.Collections.Native/TypeHelper.cs
+++ Point.Collections.Native/TypeHelper.cs
@@ -1,4 +1,5 @@
-﻿// Copyright 2021 Seung Ha Kim
+﻿// Copyright 2021 Ikina Games
+// Author : Seung Ha Kim (Syadeu)
 // 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
